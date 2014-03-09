4a4ba54833707ad34452f520fa38e410b0c1f351
Brian Spiering <bspiering@gmail.com>
2014-03-08 22:52:48 -0800

    Clean and added more use cases

--- Node.py
+++ Node.py
@@ -18,14 +18,16 @@ L = Node("a", Node("b", Node("c", Node("d"))))
 # print("'%s' is the 1st item in linked list.") % (L.value)
 # print("'%s' is the 2nd item in linked list") % (L.next.value)
 # print("'%s' is the 4nd item in linked list") % (L.next.next.next.value)
+# L = Node("a") # Shorted linked list
+# L = Node("a", Node("b", Node("c", Node("d", Node("e"))))) # Longer linked list
 
-# Walk through linked list
-for i in xrange(10): # TODO: make a while loop
+# Walk through the linked list
+for i in xrange(10000000000000000): # A kludge, the linked list will never be that big
 
-	location = "L."+"next."*i+"value" # Find based number of nexts/links
+	location = "L."+"next."*i+"value" # Find location based number of nexts/links
 
 	try: 		
-		print("'%s' is the number %d item in linked list.") % (eval(location), i+1)
-	except:
-		# print "past end of linked list"
+		print("'%s' is item number %d in linked list.") % (eval(location), i+1)
+	except AttributeError:
+		# print "Past end of current linked list."
 		break
