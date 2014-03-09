62a3b00ab061c1cc56094bed93bfdf120a5d6867
Brian Spiering <bspiering@gmail.com>
2014-03-08 21:24:59 -0800

    Initial commit
    Started on Chapter 5

---
+++ listing_5_1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+""" Exploring code from Listing 5-1 on page 105 
+"""
+
+def walk(G, s, S=set()):
+	""" Directly from Python Algorithms 
+	"""
+	P, Q = dict(), set()
+	P[s] = None
+	Q.add(s)
+	while Q:
+		u = Q.pop()
+		for v in G[u].difference(P, S):
+			Q.add(v)
+			P[v] = u
+	return P
+
+# My own work
+G = [1,2]
+u = 1
+test = walk(G, u)
+print(test)
