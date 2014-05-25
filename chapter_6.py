e6526733b01af4bcb8d88a73e174afeccf6157e4
Brian Spiering <bspiering@gmail.com>
2014-05-25 11:15:08 -0700

    Add huffman's algorithm

--- chapter_6.py
+++ chapter_6.py
@@ -5,8 +5,30 @@
 Heaps and Greedy Algorithms 
 """
 
-import heapq
+# from p 158
+from heapq import heapify, heappush, heappop
+from itertools import count
 
-# heap = []            # creates an empty heap
-# heappush(heap, item) # pushes a new item on the heap
-# item = heappop(heap) # pops the smallest item from the heap
+def huffman(seq, frq):
+    num = count()
+    trees = list(zip(frq, num, seq))
+    heapify(trees)
+    while len(trees) > 1:
+        fa, _, a = heappop(trees)
+        fb, _, b = heappop(trees)
+        n = next(num)
+        heappush(trees, (fa+fb, n, [a, b]))
+    return trees[0][-1]
+
+seq_book = "abcdefghi"
+frq_book = [4, 5, 6, 9, 11, 12, 15, 16, 20]
+tree_book = huffman(seq, frq)
+
+# my version
+import string
+import random
+
+n = 10
+seq_mine = string.letters[:n]
+frq_mine = [random.randint(1, 10) for _ in xrange(n)]
+tree_mine = huffman(seq, frq)
