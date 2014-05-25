6020c5b0cd236c1e04b147c3669efda3cfcb86ec
Brian Spiering <bspiering@gmail.com>
2014-05-25 10:24:53 -0700

    Initial commit

---
+++ chapter_6.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+""" Explore Chapter 6 of Python Algorithms
+
+Heaps and Greedy Algorithms 
+"""
+
+import heapq
+
+# heap = []            # creates an empty heap
+# heappush(heap, item) # pushes a new item on the heap
+# item = heappop(heap) # pops the smallest item from the heap
